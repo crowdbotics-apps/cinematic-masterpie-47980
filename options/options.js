6bd6db1a5c6ef033a0c4a647fb243482b270e219
Crowdbotics <team@crowdbotics.com>
2024-05-16 02:13:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cinematic-masterpie-47980",
+  url: "https://cinematic-masterpie-47980.botics.co",
+  api: "https://cinematic-masterpie-47980.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
